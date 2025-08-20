13852ef30c708360b4a79a03310976ba6e9302d1
unknown <brendoncordova13@gmail.com>
2025-08-20 01:55:03 -0300

    Exercício 05 pronto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ex02();
+        ex06();
     }
+
     static void ex01(){
         System.out.println("Bem-vindo ao Sistema de Geração de Jogadores!");
     }
+
     static void ex02(){
         String nomeCompleto = "Maria Silva";
         String cargo = "Analista de Sistemas";
@@ -32,5 +34,52 @@ public class Main {
         System.out.println("Código: " + codigo);
         System.out.println("Preço: " + preco);
         System.out.println("Promoção: " + promocao);
+    }
+    static void ex04(){
+        Scanner read = new Scanner(System.in);
+        System.out.print("Qual o valor da sua compra? R$");
+        double valor = read.nextDouble();
+        double desconto = 0.1;
+
+        if(valor > 100){
+            double valorFinal = (valor - (valor * desconto));
+            System.out.print("Valor final com desconto: R$" + valorFinal);
+        }
+        else{
+            System.out.print("Valor final sem desconto: R$" + valor);
+        }
+    }
+    static void ex05(){
+        Scanner read = new Scanner(System.in);
+        System.out.print("Qual a sua idade? ");
+        int idade = read.nextInt();
+        if(idade < 12){
+            System.out.println("Categoria: Infantil");
+        }
+        else if (idade > 12 && idade <=17) {
+            System.out.println("Categoria: Adolescente");
+        }
+        else{
+            System.out.println("Categoria: Adulto");
+        }
+    }
+    static void ex06(){
+        int[] notas = new int[5];
+        Scanner read = new Scanner(System.in);
+
+        for (int c=0; c<=4; c++){
+            int contagem = c + 1;
+            System.out.print("Qual a " + contagem + "° nota? ");
+            int nota = read.nextInt();
+            notas[c] = nota;
+        }
+
+        for (int c=0; c<=4; c++){
+            int contagem = c + 1;
+            System.out.println(contagem + "° nota : " + notas[c]);
+        }
+
+
+
     }
 }
